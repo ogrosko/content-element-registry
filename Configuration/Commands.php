a9f573c921e273a31d848575e0dd513c9c8fc968
Samuel Mihal <mihal.samuel431@gmail.com>
2020-01-15 14:35:07 +0100

    [TASK] Added commands [CE, PageType, Plugin] - bugfixed

--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -2,27 +2,27 @@
 
 return [
     'dw:contentElement' => [
-        'class' => \Digitalwerk\DwBoilerplate\Command\CreateContentElement::class,
+        'class' => \Digitalwerk\ContentElementRegistry\Command\CreateContentElement::class,
         'schedulable' => false,
     ],
     'dw:contentElementBasic' => [
-        'class' => \Digitalwerk\DwBoilerplate\Command\CreateContentElementBasic::class,
+        'class' => \Digitalwerk\ContentElementRegistry\Command\CreateContentElementBasic::class,
         'schedulable' => false,
     ],
     'dw:contentElementAdvance' => [
-        'class' => \Digitalwerk\DwBoilerplate\Command\CreateContentElementAdvance::class,
+        'class' => \Digitalwerk\ContentElementRegistry\Command\CreateContentElementAdvance::class,
         'schedulable' => false,
     ],
     'dw:contentElementAdvanceIRRE' => [
-        'class' => \Digitalwerk\DwBoilerplate\Command\CreateContentElementAdvanceIRRE::class,
+        'class' => \Digitalwerk\ContentElementRegistry\Command\CreateContentElementAdvanceIRRE::class,
         'schedulable' => false,
     ],
     'dw:pageType' => [
-        'class' => \Digitalwerk\DwBoilerplate\Command\CreatePageType::class,
+        'class' => \Digitalwerk\ContentElementRegistry\Command\CreatePageType::class,
         'schedulable' => false,
     ],
     'dw:plugin' => [
-        'class' => \Digitalwerk\DwBoilerplate\Command\CreatePlugin::class,
+        'class' => \Digitalwerk\ContentElementRegistry\Command\CreatePlugin::class,
         'schedulable' => false,
     ],
 ];
