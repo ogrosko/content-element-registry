0aea09703bcc544c3a43fa334f7e44f14afc6c6e
Samuel Mihal <mihal.samuel431@gmail.com>
2020-03-12 15:48:58 +0100

    [TASK] Separate create commands to new extension

--- Configuration/Commands.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'dw:run' => [
-        'class' => \Digitalwerk\ContentElementRegistry\Command\CreateCommand\RunCreateCommand::class,
-        'schedulable' => false,
-    ],
-];
