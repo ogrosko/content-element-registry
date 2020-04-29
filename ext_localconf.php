f8bbb109401ba9a06c198a3d666308ca57eee090
Ingo Fabbri <inf@mmc-agentur.at>
2020-04-29 11:43:09 +0200

    [FIX] specify extension-key instead of using $_EXTKEY
    $_EXTKEY is not set anymore by TYPO3

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,59 +4,55 @@ use Digitalwerk\ContentElementRegistry\Utility\ContentElementRegistryUtility;
 
 defined('TYPO3_MODE') or die();
 
-call_user_func(
-    function ($extKey) {
+(static function ($extKey) {
+    $iconRegistry = $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Imaging\IconRegistry::class
+    );
+    $contentElementsRegistry = \Digitalwerk\ContentElementRegistry\Core\ContentElementRegistry::getInstance();
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+        ContentElementRegistryUtility::convertArrayToTypoScript(
+            $contentElementsRegistry->getBaseTypoScriptPersistenceConfig(),
+            'config.tx_extbase.persistence.classes'
+        )
+    );
 
-        $iconRegistry = $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-            \TYPO3\CMS\Core\Imaging\IconRegistry::class
+    /** @var \Digitalwerk\ContentElementRegistry\ContentElement\AbstractContentElementRegistryItem $contentElement */
+    foreach ($contentElementsRegistry->getContentElements() as $contentElement) {
+        //Register CE icon
+        $iconRegistry->registerIcon(
+            $contentElement->getIconIdentifier(),
+            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            [
+                'source' => $contentElement->getIconSrcPath(),
+            ]
         );
-        $contentElementsRegistry = \Digitalwerk\ContentElementRegistry\Core\ContentElementRegistry::getInstance();
+
+        //Register CE wizard item
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            ContentElementRegistryUtility::convertArrayToTypoScript(
+                $contentElement->getWizardPageTSconfig(),
+                'mod.wizards.newContentElement.wizardItems'
+            )
+        );
+
+        //Register CE rendering definition
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+            $extKey,
+            'setup',
+            ContentElementRegistryUtility::convertArrayToTypoScript($contentElement->getTypoScriptConfiguration())
+        );
+
+
+        //Add TypoScript setup for Extbase persistence mapping
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
             ContentElementRegistryUtility::convertArrayToTypoScript(
-                $contentElementsRegistry->getBaseTypoScriptPersistenceConfig(),
+                $contentElement->getTypoScriptPersistenceConfig(),
                 'config.tx_extbase.persistence.classes'
             )
         );
 
-        /** @var \Digitalwerk\ContentElementRegistry\ContentElement\AbstractContentElementRegistryItem $contentElement */
-        foreach ($contentElementsRegistry->getContentElements() as $contentElement) {
-            //Register CE icon
-            $iconRegistry->registerIcon(
-                $contentElement->getIconIdentifier(),
-                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-                [
-                    'source' => $contentElement->getIconSrcPath(),
-                ]
-            );
-
-            //Register CE wizard item
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-                ContentElementRegistryUtility::convertArrayToTypoScript(
-                    $contentElement->getWizardPageTSconfig(),
-                    'mod.wizards.newContentElement.wizardItems'
-                )
-            );
-
-            //Register CE rendering definition
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-                $extKey,
-                'setup',
-                ContentElementRegistryUtility::convertArrayToTypoScript($contentElement->getTypoScriptConfiguration())
-            );
-
-
-            //Add TypoScript setup for Extbase persistence mapping
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-                ContentElementRegistryUtility::convertArrayToTypoScript(
-                    $contentElement->getTypoScriptPersistenceConfig(),
-                    'config.tx_extbase.persistence.classes'
-                )
-            );
-
-            //Register CE preview template
-            $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][$contentElement->getCType()] =
-                \Digitalwerk\ContentElementRegistry\Hook\ContentElementPreviewRenderer::class;
-        }
-    },
-    $_EXTKEY
-);
+        //Register CE preview template
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][$contentElement->getCType()] =
+            \Digitalwerk\ContentElementRegistry\Hook\ContentElementPreviewRenderer::class;
+    }
+})('content_element_registry');
--- ext_tables.php
+++ ext_tables.php
@@ -3,8 +3,6 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-call_user_func(
-    function () {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_contentelementregistry_domain_model_relation');
-    }
-);
+(static function () {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_contentelementregistry_domain_model_relation');
+})();
